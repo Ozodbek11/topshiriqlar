4c66c45d3526c048ed20881356a6800439a06d28
Ozodbek Ravshanov <ozodbekravshanov2020@gmail.com>
2021-06-29 14:36:55 +0500

    Дуыыщт11

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,36 @@
+# 1
 # sum = int(input(" "))
 # for x in range(11):
 #     sum += x**2
 #     print(x)
 # print(sum)
+# 2
+# a = int(input("Son kirit: "))
+# b = int(input("Son kirit: "))
+# for x in range(b-1,a,-1):
+#     print(x)
+# 3
+# k = 1
+# a = int(input("Son kirit: "))
+# for x in range(1,a+1):
+#     k *= x
+#     print(x, k)
+# 4
+# k = 0
+# a =int(input("Son kirit: "))
+# for x in range(2, a, 2):
+#     k += x
+#     print(x, k)
+# 5
+# sum = int(input("son kirit "))
+# for x in range(sum):
+#     sum += (x+x)/sum  
+
+# print(sum)
+# 6
+from math import *
+n = int(input("Son kiriting: "))
+sum =0 
+for x in range(n+1):
+    sum += x
+print(sqrt(x))
